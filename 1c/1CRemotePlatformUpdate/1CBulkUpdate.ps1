f02e94fc7a2afeee10c13d7e6c35038297eb6e61
Sergey Bezpalov <126727815+sbezpalov@users.noreply.github.com>
2025-01-10 11:47:59 +0300

    Update 1CBulkUpdate.ps1

--- 1c/1CRemotePlatformUpdate/1CBulkUpdate.ps1
+++ 1c/1CRemotePlatformUpdate/1CBulkUpdate.ps1
@@ -68,25 +68,25 @@ function Install-1C {
 # Основной процесс установки
 $Servers | ForEach-Object {
     $DestPath = "\\$_\C$\Temp_Install\"
-    $InstallPath_x64 = "$DestPath\$Version-64\setup.exe"
     $InstallPath_x86 = "$DestPath\$Version-86\setup.exe"
+    $InstallPath_x64 = "$DestPath\$Version-64\setup.exe"
     $DestPath1cestart = "\\$_\C$\ProgramData\1C\1CEStart\"
-    $BinPath_x64 = "\\$_\C$\Program Files\1cv8\$Version\bin\"
     $BinPath_x86 = "\\$_\C$\Program Files (x86)\1cv8\$Version\bin\"
-
+    $BinPath_x64 = "\\$_\C$\Program Files\1cv8\$Version\bin\"
+    
     # Создание директории Temp_Install
     Create-Directory -Path $DestPath
 
     # Копирование дистрибутивов
-    Copy-Files -Source $SourcePath_x64 -Destination $DestPath
     Copy-Files -Source $SourcePath_x86 -Destination $DestPath
+    Copy-Files -Source $SourcePath_x64 -Destination $DestPath
 
     # Копирование конфигурации 1CEStart
-    Copy-Files -Source $Source1cestart_x64 -Destination $DestPath1cestart
     Copy-Files -Source $Source1cestart_x86 -Destination $DestPath1cestart
+    Copy-Files -Source $Source1cestart_x64 -Destination $DestPath1cestart
 
-    # Установка 1С x32
-    Install-1C -Server $_ -InstallPath $InstallPath_x86 -BinPath $BinPath_x86 -VersionType "x32"
+    # Установка 1С x86
+    Install-1C -Server $_ -InstallPath $InstallPath_x86 -BinPath $BinPath_x86 -VersionType "x86"
 
     # Установка 1С x64
     Install-1C -Server $_ -InstallPath $InstallPath_x64 -BinPath $BinPath_x64 -VersionType "x64"
