22c39c638b7deba29dfba58c7a11416f0cc824b5
Sergey Bezpalov <126727815+sbezpalov@users.noreply.github.com>
2025-03-05 15:07:00 +0300

    Update get-rate-cbr.php

--- bitrix/bx-get-rate/get-rate-cbr.php
+++ bitrix/bx-get-rate/get-rate-cbr.php
@@ -1,77 +1,88 @@
 <?php
 function GetRateFromCBR($CURRENCY) 
-      { 
-         global $DB; 
-         global $APPLICATION; 
+{
+    global $DB;
+    global $APPLICATION;
 
-         CModule::IncludeModule('currency');
-       if(!CCurrency::GetByID($CURRENCY))
-    //такой валюты нет на сайте, агент в этом случае удаляется
-       return false;
-          
-         $DATE_RATE=date("d.m.Y");//сегодня 
-         $QUERY_STR = "date_req=".$DB->FormatDate($DATE_RATE, CLang::GetDateFormat("SHORT", $lang), "D.M.Y"); 
-       
-      //делаем запрос к www.cbr.ru с просьбой отдать курс на нынешнюю дату          
-      $strQueryText = QueryGetData("www.cbr.ru", 80, "/scripts/XML_daily.asp", $QUERY_STR, $errno, $errstr); 
-      
-      //получаем XML и конвертируем в кодировку сайта          
-       $charset = "windows-1251"; 
-         if (preg_match("/<"."\?XML[^>]{1,}encoding=[\"']([^>\"']{1,})[\"'][^>]{0,}\?".">/i", $strQueryText, $matches)) 
-               { 
-                  $charset = Trim($matches[1]); 
-               } 
-         $strQueryText = eregi_replace("<!DOCTYPE[^>]{1,}>", "", $strQueryText); 
-         $strQueryText = eregi_replace("<"."\?XML[^>]{1,}\?".">", "", $strQueryText); 
-         $strQueryText = $APPLICATION->ConvertCharset($strQueryText, $charset, SITE_CHARSET); 
+    if (!preg_match('/^[A-Z]{3}$/', $CURRENCY)) {
+        // Invalid currency code format
+        return false;
+    }
 
-         require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/classes/general/xml.php");
+    CModule::IncludeModule('currency');
+    if (!CCurrency::GetByID($CURRENCY)) {
+        // Такой валюты нет на сайте, агент в этом случае удаляется
+        return false;
+    }
+    
+    $DATE_RATE = date("d.m.Y"); // Сегодня
+    $QUERY_STR = "date_req=" . $DB->FormatDate($DATE_RATE, CLang::GetDateFormat("SHORT", $lang), "D.M.Y");
 
-      //парсим XML 
-         $objXML = new CDataXML(); 
-         $res = $objXML->LoadString($strQueryText); 
-         if($res !== false) 
-                  $arData = $objXML->GetArray(); 
-               else 
-                  $arData = false; 
-          
-         $NEW_RATE=Array(); 
-       
-       //получаем курс нужной валюты $CURRENCY 
-         if (is_array($arData) && count($arData["ValCurs"]["#"]["Valute"])>0) 
-               { 
-                  for ($j1 = 0; $j1<count($arData["ValCurs"]["#"]["Valute"]); $j1++) 
-                  { 
-                     if ($arData["ValCurs"]["#"]["Valute"][$j1]["#"]["CharCode"][0]["#"]==$CURRENCY) 
-                     { 
-                        $NEW_RATE['CURRENCY']=$CURRENCY; 
-                        $NEW_RATE['RATE_CNT'] = IntVal($arData["ValCurs"]["#"]["Valute"][$j1]["#"]["Nominal"][0]["#"]); 
-                        $NEW_RATE['RATE'] = DoubleVal(str_replace(",", ".", $arData["ValCurs"]["#"]["Valute"][$j1]["#"]["Value"][0]["#"])); 
-                        $NEW_RATE['DATE_RATE']=$DATE_RATE; 
-                        break; 
-                     } 
-                  } 
-               } 
-             
-         if ((isset($NEW_RATE['RATE']))&&(isset($NEW_RATE['RATE_CNT']))) 
-            { 
+    // Делаем запрос к www.cbr.ru с просьбой отдать курс на нынешнюю дату
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://www.cbr.ru/scripts/XML_daily.asp?" . $QUERY_STR);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $strQueryText = curl_exec($ch);
+    curl_close($ch);
 
-            //курс получили, возможно, курс на нынешнюю дату уже есть на сайте, проверяем 
-                CModule::IncludeModule('currency'); 
-                $arFilter = array( 
-                           "CURRENCY" => $NEW_RATE['CURRENCY'], 
-                           "DATE_RATE"=>$NEW_RATE['DATE_RATE'] 
-                              ); 
-                     $by = "date"; 
-                     $order = "desc"; 
+    if ($strQueryText === false) {
+        // Ошибка запроса
+        return false;
+    }
 
-                     $db_rate = CCurrencyRates::GetList($by, $order, $arFilter); 
-                     if(!$ar_rate = $db_rate->Fetch()) 
-                //такого курса нет, создаём курс на нынешнюю дату 
-                           CCurrencyRates::Add($NEW_RATE); 
-                
-            } 
-             
-         //возвращаем код вызова функции, чтобы агент не "убился" 
-         return 'GetRateFromCBR("'.$CURRENCY.'");'; 
-      }
+    // Получаем XML и конвертируем в кодировку сайта
+    $charset = "windows-1251";
+    if (preg_match('/<\?XML[^>]+encoding=["\']([^"\']+)["\'][^>]*\?>/i', $strQueryText, $matches)) {
+        $charset = trim($matches[1]);
+    }
+    $strQueryText = preg_replace("/<!DOCTYPE[^>]*>/", "", $strQueryText);
+    $strQueryText = preg_replace("/<\?XML[^>]*\?>/", "", $strQueryText);
+    $strQueryText = $APPLICATION->ConvertCharset($strQueryText, $charset, SITE_CHARSET);
+
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/classes/general/xml.php");
+
+    // Парсим XML
+    $objXML = new CDataXML();
+    $res = $objXML->LoadString($strQueryText);
+    if ($res !== false) {
+        $arData = $objXML->GetArray();
+    } else {
+        $arData = false;
+    }
+
+    $NEW_RATE = array();
+
+    // Получаем курс нужной валюты $CURRENCY
+    if (is_array($arData) && isset($arData["ValCurs"]["#"]["Valute"])) {
+        foreach ($arData["ValCurs"]["#"]["Valute"] as $valute) {
+            if ($valute["#"]["CharCode"][0]["#"] === $CURRENCY) {
+                $NEW_RATE['CURRENCY'] = $CURRENCY;
+                $NEW_RATE['RATE_CNT'] = intval($valute["#"]["Nominal"][0]["#"]);
+                $NEW_RATE['RATE'] = doubleval(str_replace(",", ".", $valute["#"]["Value"][0]["#"]));
+                $NEW_RATE['DATE_RATE'] = $DATE_RATE;
+                break;
+            }
+        }
+    }
+
+    if (isset($NEW_RATE['RATE']) && isset($NEW_RATE['RATE_CNT'])) {
+        // Курс получили, возможно, курс на нынешнюю дату уже есть на сайте, проверяем
+        CModule::IncludeModule('currency');
+        $arFilter = array(
+            "CURRENCY" => $NEW_RATE['CURRENCY'],
+            "DATE_RATE" => $NEW_RATE['DATE_RATE']
+        );
+        $by = "date";
+        $order = "desc";
+
+        $db_rate = CCurrencyRates::GetList($by, $order, $arFilter);
+        if (!$ar_rate = $db_rate->Fetch()) {
+            // Такого курса нет, создаём курс на нынешнюю дату
+            CCurrencyRates::Add($NEW_RATE);
+        }
+    }
+
+    // Возвращаем код вызова функции, чтобы агент не "убился"
+    return 'GetRateFromCBR("' . $CURRENCY . '");';
+}
+?>
